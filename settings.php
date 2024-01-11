5e17766ddd3c9fdf56405c32d950b21b783b0d89
rxyzal <105778108+rizal333@users.noreply.github.com>
2024-01-11 13:50:46 +0700

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,5 @@
 <?php
 
-$useragent = "XXX";
+$useragent = "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36";
 
-$cookie = "XXX";
-
+$cookie = "pid=231818356302; reg=1; login=1; user=352223063348-103.247.13.238; show_nt=1; googtrans=/en/id; Hm_lvt_2b147ccaeef7e49f5f9553cadfdf8428=1704627506,1704633818; Hm_lpvt_2b147ccaeef7e49f5f9553cadfdf8428=1704633818; googtrans=/en/id";
